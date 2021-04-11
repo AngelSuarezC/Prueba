cc6c1750cbba1df6b66752941b242a366c7b1ad7
Angel Suarez <angelsuarezcuevas@gmail.com>
2021-04-10 21:24:59 -0500

    aclaración del control de versión

--- Prueba.R
+++ Prueba.R
@@ -1,3 +1,1 @@
-print("Este archivo fue creado dentro de Rstudio")
-print("Y ahora se localiza en GitHub")
-printf("Se ha confirmado la transferencia y es exitosa")
+print("Aclarción del control de versiones")
